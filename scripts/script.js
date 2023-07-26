79f9d176126b18ead8162652eb8ed19a4576346b
Nikita Petrov <petrov.nikita702@mail.ru>
2023-07-26 17:41:20 +0300

    editing_26_07

--- scripts/script.js
+++ scripts/script.js
@@ -8,18 +8,26 @@ burgerMenu.addEventListener("click", () => {
     menuBody.classList.toggle("header-content__menu-body_visited");
 })
 
-const menuLinks = document.querySelectorAll(".header-content__section-button[data-goto]");
+window.addEventListener("resize", () => {
+    if (window.screen.width >= 768) {
+        menuBody.classList.remove("header-content__menu-body_visited");
+        burgerMenu.classList.remove("burger-menu_active");
+        document.body.classList.remove("body__lock");
+    }
+})
 
-if(menuLinks.length > 0) {
+const menuLinks = document.querySelectorAll(".header-content__section-button[data-goto]");
+if (menuLinks.length > 0) {
     menuLinks.forEach(menuLinks => {
-        menuLinks.addEventListener("click", function(e) {
+        menuLinks.addEventListener("click", function (e) {
             const elLink = e.target;
             burgerMenu.classList.toggle("burger-menu_active");
             menuBody.classList.toggle("header-content__menu-body_visited");
-            if(elLink.dataset.goto != "title-block" && document.getElementsByClassName(elLink.dataset.goto)) {
+            document.body.classList.toggle("body__lock");
+            if (elLink.dataset.goto != "title-block" && document.getElementsByClassName(elLink.dataset.goto)) {
                 const gotoBlock = document.getElementsByClassName(elLink.dataset.goto)[0];
-                const gotoBlockValue = gotoBlock.getBoundingClientRect().top + window.scrollY - document.querySelector(".header-content").offsetHeight - 30;               
-                
+                const gotoBlockValue = gotoBlock.getBoundingClientRect().top + window.scrollY - document.querySelector(".header-content").offsetHeight - 30;
+
                 window.scrollTo({
                     top: gotoBlockValue,
                     behavior: "smooth"
